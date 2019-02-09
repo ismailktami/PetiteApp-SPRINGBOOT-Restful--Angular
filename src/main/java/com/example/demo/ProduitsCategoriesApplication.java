d9d5eb9b4de68f61de321fab8938bd905d2886b9
ktami <ktami@192.168.1.7>
2019-02-09 01:26:04 +0100

    First commit

---
+++ src/main/java/com/example/demo/ProduitsCategoriesApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitsCategoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitsCategoriesApplication.class, args);
+	}
+
+}
+
